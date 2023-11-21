8a10158066c2c1a361841c52b05dd9c6353b8673
Catarina Lima <contato.joaoplimaa@gmail.com>
2023-11-21 15:29:16 -0300

    calcula os valores do plano b

--- js/calc.js
+++ js/calc.js
@@ -14,3 +14,34 @@ document.addEventListener('DOMContentLoaded', function() {
       resultados(operadoraA, operadoraB);
       recomendar(operadoraA, operadoraB);
 });
+
+function calcplanosoperadoraA(idade, imc) {
+    const planobasico = 100 + (idade * 10 * (imc / 10));
+    const planostandard = (150 + (idade * 15)) * (imc / 10);
+    const planopremium = (200 - (imc * 10) + (idade * 20)) * (imc / 10);
+
+    return { planobasico, planostandard, planopremium };
+  }
+
+function calcplanosoperadoraB(imc) {
+    let comorbidade = 0;
+    if (imc < 18.5) {
+      comorbidade = 10;
+    } else if (imc >= 18.5 && imc < 24.9) {
+      comorbidade = 1;
+    } else if (imc >= 25 && imc < 29.9) {
+      comorbidade = 6;
+    } else if (imc >= 30 && imc < 34.9) {
+      comorbidade = 10;
+    } else if (imc >= 35 && imc < 39.9) {
+      comorbidade = 20;
+    } else {
+      comorbidade = 30;
+    }
+
+    const planobasico = 100 + (comorbidade * 10 * (imc / 10));
+    const planostandard = (150 + (comorbidade * 15)) * (imc / 10);
+    const planopremium = (200 - (imc * 10) + (comorbidade * 20)) * (imc / 10);
+
+    return { planobasico, planostandard, planopremium };
+  }
